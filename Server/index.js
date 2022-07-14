756c14c784228b8c51ccc7d7aacdde3a81cc11d3
AmodSeth <amodseth2002@gmail.com>
2022-07-14 10:18:02 +0530

    added README.md

---
+++ Server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+//cors is used for debugging the new  netwrok request modules
+
+app.use(cors());
+//  const io . orgin accepts the server request
+// if we want to use another device change the origins and the cors method
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    },
+})
+//emitting the event
+
+io.on("connection", (socket) => {
+    console.log(`USER_CONNECTED ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`USER WITH ID: ${socket.id} joined room : ${data}`)
+    });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+});
+   
+    socket.on("disconnect", () => {
+        console.log("USER_DISCONNECTED", socket.id)
+    });
+
+});
+
+
+
+
+server.listen(3001, () => {
+    console.log("SERVER_RUNNING......")
+})
